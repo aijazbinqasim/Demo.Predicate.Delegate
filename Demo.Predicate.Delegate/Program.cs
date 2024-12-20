3cea2a8efe8830de2733b1475052f233666ea136
Aijaz Bin Qasim <aijazbinqasim@hotmail.com>
2024-12-20 22:43:42 +0500

    Add project files.

---
+++ Demo.Predicate.Delegate/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Demo.Predicate.Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Predicate<string> isUpperCase = A.IsUpperCase;
+           Console.WriteLine(isUpperCase("hello")); // false
+
+           Predicate<string> auth = A.IsUserAuthenticated;
+           Console.WriteLine(auth("aijaz")); // false
+
+
+            // using lambda expression
+            Predicate<string> isUpperCaseLambda = (str) => str.Equals(str.ToUpper());
+            Console.WriteLine(isUpperCaseLambda("HELLO")); // true
+
+            // using anonymous method
+            Predicate<string> isUpperCaseAnonymous = delegate (string str) { return str.Equals(str.ToUpper()); };
+            Console.WriteLine(isUpperCaseAnonymous("hello")); // false
+
+
+            Func<string, string, bool> login = A.Login;
+
+            if (login("admin", "admin123"))
+            {
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                Console.WriteLine("Login failed");
+            }
+        }
+    }
+
+    // create static class with name A
+    public static class A
+    {
+        public static bool IsUpperCase(string str) => str.Equals(str.ToUpper());
+
+        public static bool IsUserAuthenticated(string username)
+        {
+            return username.Equals("admin");
+        }
+
+        public static bool Login(string username, string password)
+        {
+            return username.Equals("admin") && password.Equals("admin");
+        }
+    }
+}
